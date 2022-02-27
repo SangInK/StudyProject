1d05ca5fd0b31fcfa846dd1a54f3c991a53b62c1
ksi <ksi@tascorp.co.kr>
2022-02-27 22:14:25 +0900

    공부내용 복구 중

---
+++ 해외취업 ASP.NET Core 웹개발 기본 강좌/MyApp/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace MyApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; } 
+
+        public string Country { get; set; }
+    }
+}
---
+++ 해외취업 ASP.NET Core 웹개발 기본 강좌/MyApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApp.Views
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "작성이 필요한 항목입니다.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "작성이 필요한 항목입니다.")]
+        [DataType(DataType.Password)]   
+        public string Password { get; set; }
+    }
+}
